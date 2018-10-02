4fc557f3ff15f1901c78a43ce7bd122654b7987b
truonghuuthanh <truonghuuthanh95@gmail.com>
2018-10-02 11:00:34 +0700

    commit

--- TraiNghiemSangTao/TraiNghiemSangTao/Repositories/Implements/SchoolRepository.cs
+++ TraiNghiemSangTao/TraiNghiemSangTao/Repositories/Implements/SchoolRepository.cs
@@ -21,7 +21,7 @@ namespace TraiNghiemSangTao.Repositories.Implements
             List<School> schools = null;
             if (schoolDegreeId == 3)
             {
-                schools = _db.Schools.Where(s => s.SchoolDegreeId == 3 || s.SchoolDegreeId == 14 || s.SchoolDegreeId == 13).Where(s => s.DistrictId == districtId).ToList();
+                schools = _db.Schools.Where(s => s.SchoolDegreeId == 3 || s.SchoolDegreeId == 14 || s.SchoolDegreeId == 15 || s.SchoolDegreeId == 13).Where(s => s.DistrictId == districtId).ToList();
             }
             else if (schoolDegreeId == 4)
             {
